d0ab0944bb4a5e1759b05945fc905b073b99a78e
Tuan Nguyen <tuan.nv1@isofhcare.com>
2023-10-20 09:39:29 +0700

    20/10

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-
-
-
-function handleClickAuthor(){
-  const author = document.querySelector('.author');
-  author.addEventListener('click', function(){
-
-    document.querySelector('.author img').style.opacity = 0;
-    document.querySelector('.author .show-action').style.opacity = 1
-  })
+function handleClickAuthor() {
+  const author = document.querySelector(".author");
+  const music = document.querySelector(".music");
+  author.addEventListener("click", function () {
+    document.querySelector(".author img").style.opacity = 0;
+    document.querySelector(".author .show-action").style.opacity = 1;
+    music.volume = 1;
+    music.play();
+  });
 }
 
-handleClickAuthor()
+handleClickAuthor();
